cd0543b5051cf223df0d80bb659bc49995faf90e
aidang18 <g00272892@stu.raritanval.edu>
2022-12-04 20:47:48 -0500

    added RPS function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import time 
+import random
 from discord import app_commands
 from discord.ext import commands
 
@@ -59,5 +60,44 @@ async def countLetters(ctx, arg):
     count = len(arg)
 
     await ctx.reply("There are " + str(count) + " letters in the word " + arg) 
+
+
+@bot.group(with_app_command=True, fallback="rps_rock")
+async def rps_rock(ctx):
+    computer=random.choice(['rock','paper','scissors'])
+    if "scissors" == computer:
+        await ctx.reply("You chose rock.\nThe Bot chose " + computer)
+        await ctx.reply("You Won!!")
+    elif "paper" == computer:
+        await ctx.reply("You chose rock.\nThe Bot chose " + computer)
+        await ctx.reply("The Bot Won!!")
+    else:
+        await ctx.reply("You chose rock and the Bot chose " + computer + "\nIt's a tie!!")
+
+
+@bot.group(with_app_command=True, fallback="rps_paper")
+async def rps_paper(ctx):
+    computer=random.choice(['rock','paper','scissors'])
+    if "rock" == computer:
+        await ctx.reply("You chose paper.\nThe Bot chose " + computer)
+        await ctx.reply("You Won!!")
+    elif "scissors" == computer:
+        await ctx.reply("You chose paper.\nThe Bot chose " + computer)
+        await ctx.reply("The Bot Won!!")
+    else:
+        await ctx.reply("You chose paper and the Bot chose " + computer + "\nIt's a tie!!")
+
+
+@bot.group(with_app_command=True, fallback="rps_scissors")
+async def rps_scissors(ctx):
+    computer=random.choice(['rock','paper','scissors'])
+    if "paper" == computer:
+        await ctx.reply("You chose scissors.\nThe Bot chose " + computer)
+        await ctx.reply("You Won!!")
+    elif "rock" == computer:
+        await ctx.reply("You chose scissors.\nThe Bot chose " + computer)
+        await ctx.reply("The Bot Won!!")
+    else:
+        await ctx.reply("You chose scissors and the Bot chose " + computer + "\nIt's a tie!!")
 
 bot.run(TOKEN)
